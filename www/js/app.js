273497625adee0cfc661d00b5223868e47576ad1
baitianxin111 <495017135@qq.com>
2017-09-20 20:53:44 +0800

    chajian

--- www/js/app.js
+++ www/js/app.js
@@ -65,8 +65,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     })
     .state('tab.chat-detail', {
       url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
+      views: {        'tab-chats': {
           templateUrl: 'templates/chat-detail.html',
           controller: 'ChatDetailCtrl'
         }
