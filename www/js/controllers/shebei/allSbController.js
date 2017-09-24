8dd5ee69b650a33806f2e7efa7510a0e7d1032be
baitianxin111 <495017135@qq.com>
2017-09-24 20:42:38 +0800

    9-24 dengguang

--- www/js/controllers/shebei/allSbController.js
+++ www/js/controllers/shebei/allSbController.js
@@ -2,7 +2,7 @@
  * Created by suc on 2017/9/22.
  */
 define(['app'],function (app) {
-  app.controller('allSbCtrl',function ($state,$scope,$ionicViewSwitcher,$ionicPlatform) {
+  app.controller('allSbCtrl',function ($state,$scope,$ionicViewSwitcher,$ionicPlatform,$stateParams) {
       $scope.goBack = function () {
         $state.go('index.shebei');
         $ionicViewSwitcher.nextDirection('back');
@@ -10,6 +10,60 @@ define(['app'],function (app) {
       $ionicPlatform.registerBackButtonAction(function () {
         $scope.goBack();
       },999)
+    //数据模拟
+    $scope.leftMenu = [
+      {
+        name :"一层",
+        selected :false,
+        children :[
+          {name:"会议室1"},
+          {name:"会客室"},
+          {name:"大厅"}
+        ]
+      },
+      {
+        name:"二层",
+        selected:false,
+        children:[
+          {name:"会议室2"},
+          {name:"会客室2"},
+          {name:"大厅2"}
+        ]
+      }
+
+    ];
+    $scope.rightList = [
+      {name :"灯光1"},
+      {name :"灯光2"}
+    ];
+    $scope.model = {
+      type : $stateParams.type
+    }
+    $scope.changeType = function (type) {
+      $scope.model.type = type ;
+      if(type == '窗帘'){
+        $scope.rightList = [
+          {name:"窗帘1"},
+          {name:"窗帘2"},
+        ]
+      }
+      else if(type == '空调'){
+        $scope.rightList = [
+          {name:"空调1"},
+          {name:"空调2"},
+        ]
+      }
+     else if(type == '灯光'){
+        $scope.rightList = [
+          {name:"灯光1"},
+          {name:"灯光2"},
+        ]
+      };
+
+    }
+    $scope.openDateil = function () {
+
+    }
   })
 
 })
